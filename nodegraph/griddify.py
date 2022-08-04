9b9ea2f1fb9e98f1e2859add475f2908a0a3b6ed
Lukas Wieg <lukaswieg@googlemail.com>
2022-08-04 22:27:33 +0200

    Add min distance and improve position access

--- nodegraph/griddify.py
+++ nodegraph/griddify.py
@@ -1,8 +1,19 @@
 """Tool for easy alignment of nuke nodes"""
-from typing import List
+import dataclasses
+from typing import List, Union
 
 import nuke
 
+@dataclasses.dataclass
+class Vector:
+    x: Union[int, float]
+    y: Union[int, float]
+
+    def __len__(self):
+        difference(self.x, self.y)
+
+
+MIN_DIST = Vector(60, 30)
 
 def difference(x1, x2):
     """Calculate the difference between two numbers"""
@@ -19,28 +30,35 @@ def griddify(ref_node: nuke.Node, nodes: List[nuke.Node] = []):
         nodes: connected nodes to align
     """
     nodes = nodes or [ref_node.input(i) for i in range(ref_node.inputs())]
-    rx = ref_node.knob("xpos").value()
-    ry = ref_node.knob("ypos").value()
+    rx = ref_node.xpos()
+    ry = ref_node.ypos()
     rw = ref_node.screenWidth() / 2
     rh = ref_node.screenHeight() / 2
 
-    for node in nodes:
+    for index, node in enumerate(nodes):
         if not node:
             continue
         if node in run.touched:
             continue
 
-        x = node.knob("xpos").value()
-        y = node.knob("ypos").value()
+        x = node.xpos()
+        y = node.ypos()
         w = node.screenWidth() / 2
         h = node.screenHeight() / 2
+        len_y = difference(y, ry)
+        len_x = difference(x, rx)
+        only_vertical = index == 0 and node.Class() != "Dot"
         if x + w == rx + rw or y + h == ry + rh:
             # skip already aligned nodes
             pass
-        elif difference(x, rx) < difference(y, ry):
-            node.knob("xpos").setValue(rx + (rw - w))
+        elif len_x < len_y or only_vertical:
+            node.setXpos(int(rx + (rw - w)))
+            if len_y < MIN_DIST.y:
+                node.setYpos(node.ypos() - MIN_DIST.y)
         else:
-            node.knob("ypos").setValue(ry + (rh - h))
+            node.setYpos(int(ry + (rh - h)))
+            if len_x < MIN_DIST.x:
+                node.setXpos(node.xpos() + MIN_DIST.x)
 
         run.touched.append(node)
         next_nodes = [node.input(i) for i in range(node.inputs())]
